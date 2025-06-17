8ff49a268a7a7576e26bffcff3575191dc2e2a80
Виктория Лебедева <victoria@MacBook-Air-Viktoria.local>
2025-06-17 11:03:11 +0200

    add utils.c

---
+++ utils.c
@@ -0,0 +1,54 @@
+#include "cub3d.h"
+
+t_img	*mlx_load_img(void *ptr, char *path)
+{
+    int     fd;
+    t_img   *i;
+    i = malloc(sizeof(t_img));
+    i->i = NULL;
+    if (!path || ft_strrncmp(path, ".xpm", 4))
+        return (i);
+    fd = open(path, O_RDONLY);
+    if (fd < 0)
+        return (i);
+    clode(fd);
+    i->i = mlx_xpm_file_to_image(ptr, path, &i->width, &i->height);
+    i->addr = mlx_get_data_addr(i->i, &i->bpp, &i->line_len, &i->endian);
+    return (i);
+}
+
+float degree_to_radians(float degree)
+{
+    return (degree * M_PI / 180);
+}
+
+int atoi_cub(const char *nptr, long *nbr)
+{
+    *nbr = 0;
+    if (!nptr || !ft_isdigit(*nptr))
+        return (-1);
+    while (ft_isdigit(*nptr))
+    {
+        *nbr = 10 * (*nbr) + (*nptr - '0');
+        if (*nbr < 0 || *nbr > 255)
+            return (-1);
+        nptr++;
+    }
+    if (*nptr)
+        retrun (-1);
+    return (0);
+}
+
+int	check_if_c(char *m, char c, int n0, int n1)
+{
+	int	i;
+
+	i = n0;
+	while (i >= 0 && i < n1 && i < (int) ft_strlen(m))
+	{
+		if (m[i] != c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
