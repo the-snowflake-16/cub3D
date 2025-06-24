e827f26656d6cb3e15df0aa00ffcf5ce70eaabff
vicmiles <57912876+vicmiles@users.noreply.github.com>
2025-06-24 15:19:03 +0200

    Update color_utils.c

--- color_utils.c
+++ color_utils.c
@@ -44,3 +44,32 @@ void	ft_free_matranspix(char ***matrix)
 	free(*matrix);
 	*matrix = NULL;
 }
+
+char	**ft_extend_matrix(char **in, char *newstr)
+{
+	char	**out;
+	int		len;
+	int		i;
+
+	i = -1;
+	out = NULL;
+	if (!newstr)
+		return (in);
+	len = ft_matrixlen(in);
+	out = malloc(sizeof(char *) * (len + 2));
+	out[len + 1] = NULL;
+	if (!out)
+		return (in);
+	while (++i < len)
+	{
+		out[i] = ft_strdup(in[i]);
+		if (!out[i])
+		{
+			ft_free_matrix(&in);
+			ft_free_matrix(&out);
+		}
+	}
+	out[i] = ft_strdup(newstr);
+	ft_free_matrix(&in);
+	return (out);
+}
